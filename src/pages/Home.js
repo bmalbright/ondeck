6af3a99d458e71e6154503f90f214e180c09d5ea
bmalbright <brianalbright@utexas.edu>
2022-04-01 13:18:38 -0500

    edited into on home page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -31,7 +31,7 @@ export default function Home() {
         <Row>
           <Col>
             <p className="paragraph">
-            Hi, I'm Brian Albright, and I founded On Deck Consulting in 2019 primarily as an aquatic safety and competitive swimming based consulting firm based in Austin, TX. 
+            Hi, I'm Brian Albright, and I founded On Deck Consulting in 2019 primarily as an aquatic safety and competitive swimming focused consulting firm based in Austin, TX. 
             You can read more about my 30+ year background in aquatics by clicking on the aquatics page.
             I recently pivoted to a career change to tech, taking a full stack web developer program through UT Austin. 
             As a result, I am offering web design and development services through On Deck Consulting.

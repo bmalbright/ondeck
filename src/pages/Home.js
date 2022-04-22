61ae29fc3bf6f372f44d03134bb67371fec17149
bmalbright <brianalbright@utexas.edu>
2022-04-22 13:30:15 -0500

    debugging to find a reset starting place. " "

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,11 @@
 import React from 'react';
-import {
-
-  Col,
-  Row,
-
-} from 'react-bootstrap';
-
-// import Logo from "../images/ondeck-icon-180.png";
-
+import { Col, Row} from 'react-bootstrap';
 import "../css/App.css";
+
 
 export default function Home() {
   return (
     <div className="home">
-
-        
-        {/* <Row>
-      
-          <Col className="profile">
-          <img className="img-responsive" src={Logo} alt="logo" />
-          </Col>
-        
-        </Row> */}
        
         <div>
 
